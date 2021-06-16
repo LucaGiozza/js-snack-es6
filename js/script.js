580487989eb48ff3cf1331a433169813ad4780c8
Luca Giozza <luca.giozza@gmail.com>
2021-06-16 15:16:29 +0200

    prova secondo esercizio

--- js/script.js
+++ js/script.js
@@ -1,55 +1,55 @@
-// Snack 1
+//  Snack 1
 
-// Creare un array di oggetti: Ogni oggetto descriverà una 
-// bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici 
-// con peso minore utilizzando destructuring e template literal
-
-
-const bici = [
-    {
-    nome : 'x2000',
-     peso : 8
-
-    },
-    {
-     nome : 'fusion',
-     peso : 7
-
-    },
-    {
-     nome: 'sonic',
-     peso : 5
-
-    }
+//  Creare un array di oggetti: Ogni oggetto descriverà una 
+//  bici da corsa con le seguenti proprietà: nome e peso. Stampare a schermo la bici 
+//  con peso minore utilizzando destructuring e template literal
 
 
- ];
+ const bici = [
+     {
+     nome : 'x2000',
+      peso : 8
 
+     },
+     {
+      nome : 'fusion',
+      peso : 7
 
- let pesoMin = bici[0];
+     },
+     {
+      nome: 'sonic',
+      peso : 5
 
- for( let i = 1; i < bici.length; i++){
-     if(bici[i].peso < pesoMin.peso){
-         pesoMin = bici[i];
      }
- }
-const {peso,nome}= bici;
- console.log(nome,peso);
- console.log(pesoMin)
+
+
+  ];
+
+
+  let pesoMin = bici[0];
+
+  for( let i = 1; i < bici.length; i++){
+      if(bici[i].peso < pesoMin.peso){
+          pesoMin = bici[i];
+      }
+  }
+ const {peso,nome}= bici;
+  console.log(nome,peso);
+  console.log(pesoMin)
 
 
  
 
- document.getElementById('container').innerHTML = 
+  document.getElementById('container').innerHTML = 
 
- `
- <ul>
-   <li> ${bici}</li>
- </ul>  
+  `
+  <ul>
+    <li> ${bici}</li>
+  </ul>  
 
- `;
+  `;
 
-// Snack2 
+ Snack2 
 
 // Creare un array di oggetti di squadre di calcio. Ogni squadra avrà diverse proprietà: nome, punti fatti,
 //  falli subiti. Nome sarà l’unica proprietà da compilare, le altre saranno tutte settate a 0.
@@ -57,3 +57,57 @@ const {peso,nome}= bici;
 //  Infine usando la destrutturazione creiamo un nuovo array i cui elementi sono sempre degli oggetti 
 //  che contengono solo nomi e falli subiti e stampiamo tutto in console.
 
+var squadre = [
+        {
+        'nome' : 'Milan',
+         'puntiFatti' : 0,
+         'falliSubiti' : 0
+     
+        },
+        {
+         'nome' : 'Roma',
+         'puntiFatti' : 0,
+         'falliSubiti' : 0
+     
+        },
+        {
+         'nome' : 'Fiorentina',
+         'puntiFatti' : 0,
+         'falliSubiti' : 0
+     
+        }
+     
+     
+     ];
+    
+    
+       var punti = squadre.punti;
+       var subiti = squadre.falli;
+    
+    
+//    uso le arrow function
+    
+ const numeriRandom = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+       
+
+    for( var i = 0; i < squadre.length; i++){
+     var puntiFattiRandom = numeriRandom(0, 5);
+     var falliSubitiRandom = numeriRandom(0, 5);
+     squadre[i].puntiFatti = puntiFattiRandom;
+     squadre[i].falliSubiti = falliSubitiRandom;
+    
+    }
+    
+    
+    
+     console.log(squadre);
+
+
+
+     const newSquadre = [];
+
+     for( let i = 0;  i < squadre.length; i++){
+         const{nome, falliSubiti} = squadre[1]
+     }
+
+     console.log(newSquadre);
